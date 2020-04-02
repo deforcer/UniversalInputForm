cd377c80a923901a457d9ca9d5f4ef4eabee952e
Alexey Dmitriev <alexey@dmitriev.info>
2020-04-02 12:42:16 +0300

    fix some issues

--- app/src/main/java/ru/deforce/universalinputform/MainActivity.java
+++ app/src/main/java/ru/deforce/universalinputform/MainActivity.java
@@ -2,6 +2,7 @@ package ru.deforce.universalinputform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -9,33 +10,41 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText text1;
+    private EditText text2;
+    private TextView resText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    public void initViews() {
+        text1 = findViewById(R.id.name);
+        text2 = findViewById(R.id.email);
+        resText = findViewById(R.id.textView);
     }
 
     public void onOkClick(View v) {
-        EditText text1 = findViewById(R.id.name);
-        EditText text2 = findViewById(R.id.email);
-        TextView resText = findViewById(R.id.textView);
 
         String name = text1.getText().toString();
         String email = text2.getText().toString();
 
         if ((name.trim().length() == 0) || (email.trim().length() == 0)) {
-            resText.setText("Заполните все поля пожалуйста и повторите попытку");
+            resText.setText(R.string.emptyfields);
         } else {
-            resText.setText("Подписка на рассылку успешно оформлена для пользователя " + name + " на электронный адрес " + email);
+            resText.setText(getString(R.string.success_message, name, email));
         }
 
     }
 
     public void onClearClick(View v) {
-        EditText text1 = findViewById(R.id.name);
-        EditText text2 = findViewById(R.id.email);
         text1.setText("");
         text2.setText("");
+        resText.setText("");
     }
 
 }
